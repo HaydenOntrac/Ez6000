dbf51ca4b51fe2d96324c9336f5ad9be546a665e
HaydenOntrac <haydenlimontrac@gmail.com>
2024-11-19 12:45:54 +1100

    Update Ez6000.py

--- Ez6000.py
+++ Ez6000.py
@@ -352,8 +352,10 @@ if calculate_button:
             if df is not None:
                 st.title('Bucket Sizing and Productivity Calculator')
                 st.dataframe(df)
-                st.write(f"*Dump Truck fill factor of {(100*dump_truck_payload_new/dump_truck_payload):.0f}% applied for XMOR® Bucket pass matching.")
-                st.write(f"*Dump Truck fill factor of {(100*dump_truck_payload_old/dump_truck_payload):.0f}% applied for Old Bucket pass matching.")
+                if dump_truck_payload_new != dump_truck_payload:
+                    st.write(f"*Dump Truck fill factor of {(100*dump_truck_payload_new/dump_truck_payload):.0f}% applied for XMOR® Bucket pass matching.")
+                if dump_truck_payload_old != dump_truck_payload:
+                    st.write(f"*Dump Truck fill factor of {(100*dump_truck_payload_old/dump_truck_payload):.0f}% applied for Old Bucket pass matching.")
                 excel_file = generate_excel(df)
                 st.download_button(
                     label="Download Results In Excel",
