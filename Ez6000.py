f61c6640a4236c97ae318cb95cb0acbf87b7571e
HaydenOntrac <haydenlimontrac@gmail.com>
2024-11-19 13:59:14 +1100

    Update Ez6000.py

--- Ez6000.py
+++ Ez6000.py
@@ -217,7 +217,7 @@ if calculate_button:
             st.success(f"Good news! ONTRAC could improve your productivity!")
             st.success(f"Your ONTRAC XMOR® Bucket Solution is the: {optimal_bucket['bucket_name']} ({optimal_bucket['bucket_size']} m³)")
             st.write(f"Total Suspended Load (XMOR® Bucket): {optimal_bucket['total_bucket_weight']:.0f}kg")
-            st.write(f"Safe Working Load at {user_data['reach']}m reach ({user_data['make']} {user_data['model']}): {swl}kg")
+            st.write(f"Safe Working Load at {user_data['reach']}m reach ({user_data['make']} {user_data['model']}): {swl:.0f}kg")
             st.write(" ")
             st.write(f"Calculations based on the {user_data['make']} {user_data['model']} with a {user_data['boom_length']}m boom, {user_data['arm_length']}m arm, {user_data['cwt']}kg counterweight, {user_data['shoe_width']}mm shoes, operating at a reach of {user_data['reach']}m, and with a material density of {user_data['material_density']:.0f}kg/m³.")
             st.write(" ")
